11b8f4f248072b606ba85b9173a88ec6c513e7eb
Kawtar CHOUBARI <choubchouba.kawtar@gmail.com>
2021-03-13 20:05:39 +0100

    basic player

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-
+import React from "react";
+import Player from "./components/Player";
+import Song from "./components/Song";
+import "./styles/app.scss";
 
 function App() {
   return (
     <div>
-      <h1>Hello World !</h1>
+      <Song />
+      <Player />
     </div>
   );
 }
---
+++ src/components/Song.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Song = () => {
+  return (
+    <div className="song-container">
+      <h1>Picture</h1>
+      <h1>Song Name</h1>
+      <h1>Artist</h1>
+    </div>
+  );
+};
+
+export default Song;
