fe2d63d971f097d631453608b96d2d23fbf7cee4
Kawtar CHOUBARI <choubchouba.kawtar@gmail.com>
2021-04-02 15:25:36 +0100

    writing Readme, adding pictures

--- README.md
+++ README.md
@@ -1,3 +1,30 @@
-# Music App
+<h2 align="center">Chill 💿 Choub</h2>
 
-Music Player App built with React.
+---
+
+<p align="center">🎵 Your favorite LoFi Music Player🎧</p>
+<p align="center">⭐⭐⭐⭐⭐</a></p>
+
+<br>
+
+- A Responsive Web-App to enjoy and chill Lofi Beats.
+- Developed using ReactJS.
+- Beautiful styling using SASS.
+- Music Collected manualy from [ChillHop](https://chillhop.com/) and stored in [playlist.js](/playlist.js).
+- Deployed on Vercel or Netlify
+- [Live DEMO]()
+
+<br>
+
+---
+
+<br>
+<p align="center">
+  <img  src="screenshots/player.PNG">
+  <img  src="screenshots/library.PNG">
+</p>
+
+---
+
+<p align="right">
+<a href="https://www.buymeacoffee.com/choubari" target="_blank"><img src="https://cdn.buymeacoffee.com/buttons/lato-orange.png" alt="Buy Me A Coffee" width="140px" heigh="50px" ></a>
--- src/playlist.js
+++ src/playlist.js
@@ -2,6 +2,15 @@ import { v4 as uuidv4 } from "uuid";
 function chillHop() {
   return [
     {
+      name: "Oasis",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/11/f78c39b4bb6313ddd0354bef896c591bfb490ff8-1024x1024.jpg",
+      artist: "Makzo",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=11768",
+      color: ["#47609D", "#E88774"],
+      id: uuidv4(),
+    },
+    {
       name: "Beaver Creek",
       cover:
         "https://chillhop.com/wp-content/uploads/2020/09/0255e8b8c74c90d4a27c594b3452b2daafae608d-1024x1024.jpg",
@@ -32,6 +41,60 @@ function chillHop() {
       active: false,
     },
     {
+      name: "Going Back",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/10/737bb830d34592344eb4a2a1d2c006cdbfc811d9-1024x1024.jpg",
+      artist: "Swørn",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=10310",
+      color: ["#335561", "#3A393E"],
+      id: uuidv4(),
+    },
+    {
+      name: "Bliss",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/09/5bff1a6f1bd0e2168d29b4c841b811598135e457-1024x1024.jpg",
+      artist: "Misha, Jussi Halme",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=9248",
+      color: ["#2A416D", "#E98087"],
+      id: uuidv4(),
+    },
+    {
+      name: "Growing Apart",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/07/ff35dede32321a8aa0953809812941bcf8a6bd35-1024x1024.jpg",
+      artist: "Swørn",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=9219",
+      color: ["#BD3D76", "#551853"],
+      id: uuidv4(),
+    },
+    {
+      name: "Sails",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/06/49f6e32ca521fbad46a1b281e3893cf6254bf11d-1024x1024.jpg",
+      artist: "Strehlow, Aylior",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=9355",
+      color: ["#F2C5AB", "#BE5853"],
+      id: uuidv4(),
+    },
+    {
+      name: "Cruisin'",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/07/8404541e3b694d16fd79433b142ed910f36764dd-1024x1024.jpg",
+      artist: "Cloudchord, G Mills",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=8200",
+      color: ["#FF194A", "#41B3B3"],
+      id: uuidv4(),
+    },
+    {
+      name: "Maple Leaf Pt.2",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/09/2899f7cc22ab12e17d0119819aac3ca9dbab46e6-1024x1024.jpg",
+      artist: "Philanthrope",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=10243",
+      color: ["#CA483B", "#682C24"],
+      id: uuidv4(),
+    },
+    {
       name: "Nightfall",
       cover:
         "https://chillhop.com/wp-content/uploads/2020/07/ef95e219a44869318b7806e9f0f794a1f9c451e4-1024x1024.jpg",
@@ -52,6 +115,33 @@ function chillHop() {
       active: false,
     },
     {
+      name: "Leaving For Good",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/07/7a84488fd87082302cb69c05262f2f3f87e93018-1024x1024.jpg",
+      artist: "Hanz",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=8264",
+      color: ["#90B4AA", "#F2AE87"],
+      id: uuidv4(),
+    },
+    {
+      name: "Eastway",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/07/c572841e8431cebc120dffed4f92119f723dd954-1024x1024.jpg",
+      artist: "Dontcry, Nokiaa",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=9071",
+      color: ["#B442A1", "#F54F7A"],
+      id: uuidv4(),
+    },
+    {
+      name: "Wake up",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/07/2c3bd458bfb0713c89f991d1ce469523e95e3b53-1024x1024.jpg",
+      artist: "Evil Needle",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=8285",
+      color: ["#A35CA0", "#EE8D66"],
+      id: uuidv4(),
+    },
+    {
       name: "Under the City Stars",
       cover:
         "https://chillhop.com/wp-content/uploads/2020/09/0255e8b8c74c90d4a27c594b3452b2daafae608d-1024x1024.jpg",
@@ -69,6 +159,15 @@ function chillHop() {
       color: ["#74c7b4", "#1d6d92"],
       id: uuidv4(),
       active: false,
+    },
+    {
+      name: "Deeper",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/10/23fdd99adc3e16abcb67b004ea3e748ebf433a49-1024x1024.jpg",
+      artist: "Aviino",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=10460",
+      color: ["#C668B2", "#4D92D7"],
+      id: uuidv4(),
     },
 
     //ADD MORE HERE
